b04bd79fb2924cbaacfe97db0fac622e3f2055a5
PiyushAnand-3135 <149292519+PiyushAnand-3135@users.noreply.github.com>
2024-07-28 21:00:00 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,112 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+
+root = Tk()
+root.geometry("1080x620")
+root.title("NoteNinja - The OpenSource text editor")
+
+saved = False  # Ensure saved is defined in the global scope
+file_path = None  # Keep track of the file path
+
+
+def is_text_area_empty():
+    content = text_area.get("1.0", "end-1c").strip()
+    return len(content) == 0
+
+
+def new_file():
+    if saved or is_text_area_empty():
+        text_area.delete("1.0", END)
+    else:
+        new_confirm = messagebox.askyesno("Unsaved Changes",
+                                          "Are you sure you want to create new file without saving changes?")
+        if new_confirm:
+            text_area.delete("1.0", END)
+
+
+def open_file():
+    file_path = filedialog.askopenfilename(defaultextension=".txt",
+                                           filetypes=[("Text files", "*.txt"), ("All Files", "*.*")])
+    if file_path:
+        with open(file_path, 'r') as f:
+            content = f.read()
+            text_area.delete("1.0", "end")
+            text_area.insert("1.0", content)
+
+
+def save_file():
+    global saved, file_path
+    if not saved:
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt",
+                                                 filetypes=[("Text files", "*.txt"), ("All Files", "*.*")])
+        if file_path:
+            saved = True
+
+    if saved and file_path:
+        with open(file_path, 'w') as f:
+            f.write(text_area.get("1.0", "end-1c"))
+
+def save_as():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt",
+                                             filetypes=[("Text files", "*.txt"), ("All Files", "*.*")])
+    if file_path:
+        saved = True
+        with open(file_path, 'w') as f:
+            f.write(text_area.get("1.0", "end-1c"))
+
+def exit():
+    if saved or is_text_area_empty():
+        root.quit()
+    else:
+        quit_confirm = messagebox.askyesno("Unsaved Changes", "Are you sure you want to exit without saving changes?")
+        if quit_confirm:
+            root.quit()
+
+def about():
+    messagebox.showinfo("About NoteNinja", "NoteNinja is a text editor made by Piyush Anand")
+
+def report_problem():
+    messagebox.showinfo("Report a problem", "Write a mail on anandpiyush404@gmail.com")
+# Frame for the menu bar
+menu_frame = Frame(root)
+menu_frame.pack(side=TOP, fill=X)
+
+# MenuBar
+my_menu = Menu(root)
+
+# File Menu
+file_menu = Menu(my_menu, tearoff=False)
+file_menu.add_command(label="New", command=new_file)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_file)
+file_menu.add_command(label="Save As", command=save_as)
+file_menu.add_command(label="Exit", command=exit)
+my_menu.add_cascade(label="File", menu=file_menu)
+
+# Edit Menu
+edit_menu = Menu(my_menu, tearoff=False)
+edit_menu.add_command(label="Edit Font Size")
+my_menu.add_cascade(label="Edit", menu=edit_menu)
+
+# Help Menu
+help_menu = Menu(my_menu, tearoff=False)
+help_menu.add_command(label="About NoteNinja", command=about)
+help_menu.add_command(label="Report a problem", command=report_problem)
+my_menu.add_cascade(label="Help", menu=help_menu)
+
+# Show main menu on the app window
+root.config(menu=my_menu)
+
+# Frame for the text area
+text_frame = Frame(root)
+text_frame.pack(expand=True, fill='both')
+
+# Input text area
+text_area = Text(text_frame, wrap=WORD, undo=True, font=("Arial", 20))
+text_area.pack(expand=True, fill='both')
+
+# Set focus to the text area
+text_area.focus_set()
+
+root.mainloop()
